434e22b88291f36907b91a078a80d0f0c640a44e
Беркута Володимир Юрійович <berkutavolodymyr125@gmail.com>
2025-04-22 20:15:42 +0300

    help x2

--- Lab 5/GA.py
+++ Lab 5/GA.py
@@ -7,52 +7,53 @@ class GA:
                         max_weight: int, 
                  population_size=100, 
                  generations=1000, 
-                 mutation_rate=0.2, 
-                 tournament_size=3, 
-                 elitism=True):
+                 mutation_rate=0.1, 
+                 tournament_size=3):
 
 
         self.population_size = population_size
         self.generations = generations
         self.mutation_rate = mutation_rate
         self.tournament_size = tournament_size
-        # Використовувати елітизм чи ні
-        self.elitism = elitism
         
         # Список предметів (вага, вартість)
         self.items = items
         
-        self.min_weight = min(self.items, key=lambda x: x[0])[0]
-        
         # Максимальна вага рюкзака
         self.max_weight = max_weight
+        
+        self.w = int(self.max_weight // min(self.items, key=lambda x: x[0])[0])
+        self.w = self.w.bit_length()
+        
+        self.to_int = np.ones(self.w, dtype=int)
+        for i in range(1, self.w):
+            self.to_int[i] = 2*self.to_int[i-1]
+        self.to_int = self.to_int[::-1]
 
     # Функція пристосованості
     def fitness(self, chromosome):
-        total_weight = self.items[..., 0] @ chromosome
-        total_value = self.items[..., 1] @ chromosome
+        a = self.to_int @ chromosome
+        
+        for i in range(len(a)):
+            a[i] = np.round(a[i]/(1<<self.w) * (self.max_weight/self.items[i][0]))
+            
+        total_weight = self.items[..., 0] @ a
+        total_value = self.items[..., 1] @ a
         # Штраф за перевищення ваги
         if total_weight > self.max_weight:
-            return 0
+            return -1
         return total_value
 
     # Створення випадкової хромосоми
     def create_chromosome(self, length):
-        chromosome = np.zeros(length, dtype=int)
-        for i in range(length):
-            chromosome[i] = random.randint(0, self.max_weight//self.items[i][0])
+        chromosome = np.random.randint(0, 2, (self.w, length), dtype=int)
         return chromosome
 
     # Відбір турніром
     def tournament_selection(self, population):
-        selected = []
-        # Обираємо 2-ох батьків з турніру
-        contestants = random.sample(population, 2*self.tournament_size)
-        winners = sorted(contestants, key=lambda x: x[1])
-        
-        for i in range(2):
-            selected.append(winners[i][0])
-        return selected
+        contestants = random.sample(population, self.tournament_size)
+        winner = max(contestants, key=lambda x: x[1])
+        return winner[0]
 
     # Одноточковий кросовер
     def crossover(self, parent1, parent2):
@@ -60,28 +61,31 @@ class GA:
         child2 = np.zeros_like(parent1)
         
         for i in range(len(parent1)):
-            min_blength = self.max_weight.bit_length() + 1
+            point = random.randint(1, self.w)
             
-            point = random.randint(1, min_blength-1)
+            child1[i, :point] = parent1[i, :point]
+            child1[i, point:] = parent2[i, point:]
             
-            child1[i] = ((parent1[i] >> point) << point)\
-                ^ (parent2[i] & ((1 << point) - 1))
+            child2[i, :point] = parent1[i, :point]
+            child2[i, point:] = parent2[i, point:]
             
-            child2[i] = ((parent2[i] >> point) << point)\
-                ^ (parent1[i] & ((1 << point) - 1))
+            # child1[i] = ((parent1[i] >> point) << point)\
+            #     ^ (parent2[i] & ((1 << point) - 1))
+            
+            # child2[i] = ((parent2[i] >> point) << point)\
+            #     ^ (parent1[i] & ((1 << point) - 1))
             
             return child1, child2
         return parent1, parent2
 
     # Мутація
     def mutate(self, chromosome):
-        for i in range(len(chromosome)):
-            if random.random() < self.mutation_rate:
-                # Зміна значення хромосими
-                i_max = self.max_weight//self.items[i][0]
-                mutated_chromosome = i_max - chromosome[i]
-                mutated_chromosome = np.clip(mutated_chromosome, 0, i_max)
-                chromosome[i] = mutated_chromosome
+        if random.random() < self.mutation_rate:
+            for chrom in chromosome:
+                if random.random() < self.mutation_rate:
+                    a, b = sorted(np.random.choice(chromosome.shape[1], 2))
+                    # Зміна значення хромосими
+                    chrom[a:b] = chrom[a:b][::-1]
         return chromosome
 
     # Генетичний алгоритм
@@ -102,17 +106,16 @@ class GA:
             # Відбір та створення нового покоління
             new_population = []
             
-            if self.elitism:
-                # Елітизм: зберегти найкращу хромосому
-                elite = max(population, key=lambda x: x[1])
-                new_population.append(elite)
+            elite = max(population, key=lambda x: x[1])
+            new_population.append(elite)
             
             while len(new_population) < self.population_size:
                 # Вибір батьків
-                parents = self.tournament_selection(population)
+                parent1 = self.tournament_selection(population)
+                parent2 = self.tournament_selection(population)
                 
                 # Схрещування
-                child1, child2 = self.crossover(parents[0], parents[1])
+                child1, child2 = self.crossover(parent1, parent2)
                 
                 # Мутація
                 child1 = self.mutate(child1)
